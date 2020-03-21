b2d1f4086f5488abf13ebeb73b3e4a6760a53459
Jordan Wu <jordan.spencer.wu@gmail.com>
2020-03-20 20:18:12 -0600

    add gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,18 @@ module.exports = {
     `gatsby-plugin-less`,
     `gatsby-plugin-typescript`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Jordan Wu`,
+        short_name: `JordanWu`,
+        start_url: `/`,
+        background_color: `#000`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
