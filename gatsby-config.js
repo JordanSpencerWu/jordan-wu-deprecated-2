ee302db3fbe797d4008054ce995221c0e2baa02c
Jordan Wu <jordan.spencer.wu@gmail.com>
2020-03-20 19:43:13 -0600

    add gatsby-plugin-typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,11 @@ module.exports = {
   plugins: [
     `gatsby-plugin-less`,
     `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    }
   ],
 }
