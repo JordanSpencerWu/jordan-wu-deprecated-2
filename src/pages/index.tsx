e61f3f0e49afc5c68c9f44ce3eae3b8f414fda9f
Jordan Wu <jordan.spencer.wu@gmail.com>
2020-03-21 17:10:27 -0600

    add husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,7 @@ import Layout from "../components/layout"
 export default () => {
   return (
     <Layout>
-      <SEO
-        title="Home"
-        description="Welcome to my website!"
-      />
+      <SEO title="Home" description="Welcome to my website!" />
       <div>Hello world!</div>
     </Layout>
   )
