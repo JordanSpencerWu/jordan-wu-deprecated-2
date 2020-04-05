e046a4897e55a6b6c6be7b0362f1a34eabf833be
Jordan Wu <jordan.spencer.wu@gmail.com>
2020-04-05 12:30:15 -0600

    add gradient to canvas

--- src/components/page-contents/home-content/index.tsx
+++ src/components/page-contents/home-content/index.tsx
@@ -1,14 +1,12 @@
-import React, { ReactElement, memo, useRef } from "react"
+import React, { ReactElement, memo, useRef, Suspense } from "react"
 import { Canvas } from "react-three-fiber"
 import { Color, Uncharted2ToneMapping, Vector3 } from "three"
 
-import Particles from "./particles"
 import "./style/index.less"
 
 type HomeContentProps = {}
 
 function HomeContent(props: HomeContentProps): ReactElement {
-  const mouse = useRef([0, 0])
   const isMobile =
     typeof window !== `undefined` &&
     /iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent)
@@ -17,15 +15,12 @@ function HomeContent(props: HomeContentProps): ReactElement {
     <div className="home-content-container">
       <Canvas
         pixelRatio={Math.min(2, isMobile ? window.devicePixelRatio : 1)}
-        camera={{ fov: 100, position: new Vector3(0, 0, 30) }}
+        camera={{ fov: 100, position: new Vector3(0, 0, 10) }}
         onCreated={({ gl }) => {
           gl.toneMapping = Uncharted2ToneMapping
-          gl.setClearColor(new Color("#050505"))
         }}
       >
-        <fog attach="fog" args={["white", 50, 190]} />
         <pointLight distance={100} intensity={4} color={new Color("white")} />
-        <Particles count={isMobile ? 5000 : 10000} mouse={mouse} />
       </Canvas>
     </div>
   )
